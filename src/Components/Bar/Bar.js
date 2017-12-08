348940d93749b10648f9d0acf78394ae0a6b6250
David Paul <davidpauljunior@gmail.com>
2017-12-08 16:16:12 +0000

    Increases Y division to 8 to fix coordinate matching

--- src/Components/Bar/Bar.js
+++ src/Components/Bar/Bar.js
@@ -23,18 +23,25 @@ export default class Bar extends React.Component {
          * This is using the remainder operator to create coordinates.
          * From 0 - 7, x will be the iterator.  At 8, i becomes 0 the increments up again.  At 16 it becomes 0, then increments up again, etc.
          * It always results in a number between 0 and 7, which is the number of columns.
+         * 8 doesn't go into 0, leaves 0.
+         * 8 doesn't go into 1, leaves 1.
+         * ...
+         * 8 goes into 8, leaves 0.
+         * 9 goes into 8, leaves 1.
          * 
          * What is y?
          * This is rounding down the result of the division.
-         * From 0 - 4, y will round down to 0. From 5 - 9, y will round down to 1.  From 10 to 14, y will round down to 3 etc.
+         * From 0 - 7, y will round down to 0. From 8 - 15, y will round down to 1.  From 16 - 23, y will round down to 3 etc.
          */
         const x = i % 8;
-        const y = Math.floor(i / 5);
+        const y = Math.floor(i / 8); // Even though this is the Y axis, it needs to go up to 8, so x coord from 0 - 7 can always have a Y of 0, 8-15 can have 1. The iterator will control the point at which it stops (which is 8x5);
 
-         function renderNote(noteCoords, index) {
+        console.log(x, y);
+
+        function renderNote(noteCoords, index) {
             const [noteX, noteY] = noteCoords; // compiles down to two variables, noteX and noteY with the corresponding values from props.
             return (x === noteX && y === noteY) ? <Note key={index} /> : null; // coords  // {TODO} - Needs a key
-         }
+        }
 
         return (
             <Cell key={i}>
@@ -46,7 +53,6 @@ export default class Bar extends React.Component {
     }
 
     render() {
-
         /**
          * This loops through the number of cells in the grid
          * and calls the renderCell function for each iteration,
--- src/Components/Rhythm/Rhythm.js
+++ src/Components/Rhythm/Rhythm.js
@@ -27,7 +27,7 @@ export default class Rhythm extends Component {
                     <div>
                         <button>Play</button>
                     </div>
-                    <Bar notePositions={[[3,2], [1,3], [1,2]]}/> {/* TODO: Y coords seem to start at 1, not 0 */}
+                    <Bar notePositions={[[0,0], [1,2], [4,3]]}/>
                 </div>
             );
         } else {
